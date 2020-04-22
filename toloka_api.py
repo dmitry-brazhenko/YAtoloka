519eec835d85149b9af230d0ebb886a363f79b6b
Dmitry Brazhenko <brazhenko.dmitry@gmail.com>
2020-04-22 18:02:32 +0300

    Reformatted code

--- toloka_api.py
+++ toloka_api.py
@@ -3,60 +3,56 @@ import time
 import pandas as pd
 
 
-
 class TolokaProcessingApi:
     def __init__(self, toloka_token):
         self.toloka_token = toloka_token
-        self.headers = headers = {"Authorization":"OAuth " + toloka_token}
+        self.headers = headers = {"Authorization": "OAuth " + toloka_token}
 
     def create_toloka_project(self, project_params):
-        req = requests.post("https://toloka.yandex.ru/api/v1/projects", headers=self.headers, json = project_params)
-        assert(req.ok)
+        req = requests.post("https://toloka.yandex.ru/api/v1/projects", headers=self.headers, json=project_params)
+        assert (req.ok)
         new_project_id = req.json()['id']
         print("New project was created. New project id: ", new_project_id)
         print("https://toloka.yandex.ru/requester/project/{}".format(new_project_id))
         return new_project_id
 
-
     def create_toloka_pool(self, project_id, pool_params):
         pool_params['project_id'] = str(project_id)
-        req = requests.post("https://toloka.yandex.ru/api/v1/pools", headers=self.headers, json = pool_params)
-        assert(req.ok)
+        req = requests.post("https://toloka.yandex.ru/api/v1/pools", headers=self.headers, json=pool_params)
+        assert (req.ok)
         new_pool_id = req.json()['id']
         print("New pool was created. New pool id: ", new_pool_id)
         return new_pool_id
 
     def clone_toloka_pool(self, pool_id):
         req = requests.post("https://toloka.yandex.ru/api/v1/pools/{}/clone".format(pool_id), headers=self.headers)
-        assert(req.ok)
+        assert (req.ok)
         operation_id = req.json()['id']
         time.sleep(10)
         operation_status = requests.get("https://toloka.yandex.ru/api/v1/operations/{}".format(operation_id),
-                headers=self.headers).json()
+                                        headers=self.headers).json()
 
-        assert(operation_status['status'] == 'SUCCESS')
+        assert (operation_status['status'] == 'SUCCESS')
 
         new_pool_id = operation_status['details']['pool_id']
         print("Pool was cloned. New pool was created. New pool id: ", new_pool_id)
         print("https://toloka.yandex.ru/requester/pool/{}".format(new_pool_id))
         return new_pool_id
 
-
     def upload_toloka_tasks(self, tasks):
         req = requests.post("https://toloka.yandex.ru/api/v1/task-suites?allow_defaults=true",
-                       headers=self.headers, json = tasks)
+                            headers=self.headers, json=tasks)
         if not req.ok:
             print(req.text)
-        assert(req.ok)
+        assert (req.ok)
 
     def run_toloka_pool(self, pool_id):
         req = requests.post("https://toloka.yandex.ru/api/v1/pools/{}/open".format(pool_id), headers=self.headers)
-        assert(req.ok)
+        assert (req.ok)
         print("Pool {} was started".format(pool_id))
 
     def print_toloka_pool_link(self, pool_id):
         print("https://toloka.yandex.ru/requester/pool/{}".format(pool_id))
-
 
     def get_winner_url(self, row):
         if row['result'] == "LEFT":
@@ -75,9 +71,10 @@ class TolokaProcessingApi:
             return None
 
     def get_pool_completed_tasks(self, pool_id):
-        req = requests.get("https://toloka.yandex.ru/api/v1/assignments?pool_id={}&limit=10000".format(pool_id), headers = self.headers)
+        req = requests.get("https://toloka.yandex.ru/api/v1/assignments?pool_id={}&limit=10000".format(pool_id),
+                           headers=self.headers)
         completed_items = req.json()["items"]
-        #return completed_items
+        # return completed_items
         all_tasks = []
         all_solutions = []
         for i in completed_items:
@@ -95,12 +92,12 @@ class TolokaProcessingApi:
         df['loser_url'] = df.apply(self.get_loser_url, axis=1)
         return df
 
-
     def get_pool_params(self, pool_id):
-        req = requests.get("https://toloka.yandex.ru//api/v1/pools/{}".format(pool_id), headers = self.headers)
+        req = requests.get("https://toloka.yandex.ru//api/v1/pools/{}".format(pool_id), headers=self.headers)
         assert req.ok
         return req.json()
 
     def update_pool_params(self, pool_id, pool_params):
-        req = requests.put("https://toloka.yandex.ru//api/v1/pools/{}".format(pool_id), headers = self.headers, json=pool_params)
+        req = requests.put("https://toloka.yandex.ru//api/v1/pools/{}".format(pool_id), headers=self.headers,
+                           json=pool_params)
         assert req.ok
--- toloka_results_aggregation.py
+++ toloka_results_aggregation.py
@@ -1,34 +1,34 @@
 def prepare_toloka_task_for_2_screens(url1, url2, overlap, pool_id,
-                                      default_question = "Какой вариант вам больше нравится?"):
-    assert(overlap >=2)
+                                      default_question="Какой вариант вам больше нравится?"):
+    assert (overlap >= 2)
     tasks = []
 
     toloka_overlap = int(overlap) / 2
     tasks = [{
-            "pool_id": str(pool_id),
-            "tasks": [
+        "pool_id": str(pool_id),
+        "tasks": [
 
-                {
-                  "input_values": {
-                      "question":default_question,
-                      "image_left": url1,
-                      "image_right": url2
-                   },
-
+            {
+                "input_values": {
+                    "question": default_question,
+                    "image_left": url1,
+                    "image_right": url2
                 },
-            ],
-            "overlap": toloka_overlap,
-        },
+
+            },
+        ],
+        "overlap": toloka_overlap,
+    },
         {
 
             "pool_id": str(pool_id),
             "tasks": [
                 {
-                  "input_values": {
-                      "question":default_question,
-                      "image_left": url2,
-                      "image_right": url1
-                   },
+                    "input_values": {
+                        "question": default_question,
+                        "image_left": url2,
+                        "image_right": url1
+                    },
 
                 }
             ],
@@ -39,10 +39,9 @@ def prepare_toloka_task_for_2_screens(url1, url2, overlap, pool_id,
 
 
 def prepare_toloka_task_for_many_screens(urls_array, overlap, pool_id,
-                                      default_question = "Какой вариант вам больше нравится?"):
-    assert(overlap >=2)
+                                         default_question="Какой вариант вам больше нравится?"):
+    assert (overlap >= 2)
     tasks = []
-
 
     for left_side in urls_array:
         for right_side in urls_array:
@@ -52,11 +51,11 @@ def prepare_toloka_task_for_many_screens(urls_array, overlap, pool_id,
                     "tasks": [
 
                         {
-                          "input_values": {
-                              "question":default_question,
-                              "image_left": left_side,
-                              "image_right": right_side
-                           },
+                            "input_values": {
+                                "question": default_question,
+                                "image_left": left_side,
+                                "image_right": right_side
+                            },
 
                         },
                     ],
